172ba69ff8d542784fc46e92cef65002614a1427
hira-mushtaq1 <hira2332001@gmail.com>
2023-07-22 15:55:50 +0500

    edited main screen

--- main.dart
+++ main.dart
@@ -1,10 +1,35 @@
-import 'students.dart';
+import 'students.dart' as addStudent;
 import 'course.dart';
 import 'attendance.dart';
 import 'teachers.dart';
 import 'accountant.dart';
 import 'receptionist.dart';
+import 'dart:io';
 
+
+
+
+void main(){
+
+var userName = stdin.readLineSync();
+  var password = stdin.readLineSync();
+  bool isLogin = false;
+  while (isLogin == false) {
+    if (userName == "hira@gmail.com" && password == "123456") {
+      print("Login successful");
+      isLogin = true;
+    } else {
+      print("Loggin Failed");
+
+      userName = stdin.readLineSync();
+      password = stdin.readLineSync();
+    }
+  }
+
+
+
+
+}
 
 
 
